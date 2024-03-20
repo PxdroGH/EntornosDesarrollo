c3f40d3c0d70ff9dc05a2eda0645f69e2b63f3cf
PxdroGH <159459150+PxdroGH@users.noreply.github.com>
2024-03-20 18:21:47 +0100

    Update Clase1.java

--- src/Clase1.java
+++ src/Clase1.java
@@ -1,5 +1,6 @@
 public class Clase1 {
     public static void main(String[] args) {
         System.out.println("Soy el main de la clase 1");
+        System.out.println("Modificado desde la WEB de Github");
     }
 }
