f6b35b06887dacfc5c3befeea4056a32f4ea80fd
PxdroGH <159459150+PxdroGH@users.noreply.github.com>
2024-03-25 21:31:35 +0100

    Update Clase1.java

--- src/Clase1.java
+++ src/Clase1.java
@@ -5,5 +5,6 @@ public class Clase1 {
         System.out.println("Modificado desde la WEB de Github por segunda vez");
         System.out.println("Modificado por cuarta vez pero desde casa porque me dejé las capturas en el pc de clase");
         System.out.println("Modificado por quinta vez desde la WEB de GitHub");
+        System.out.println("Modificado por sexta vez desde la WEB de GitHub");
     }
 }
