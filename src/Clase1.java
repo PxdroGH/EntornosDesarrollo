263462654253c3350eb60e0b4f4b3ccc421fa06f
PxdroGH <pedro99cuadrado@gmail.com>
2024-03-25 19:31:19 +0100

    Commit numero 1 desde casa

--- src/Clase1.java
+++ src/Clase1.java
@@ -3,5 +3,6 @@ public class Clase1 {
         System.out.println("Soy el main de la clase 1");
         System.out.println("Modificado desde la WEB de Github");
         System.out.println("Modificado desde la WEB de Github por segunda vez");
+        System.out.println("Modificado por cuarta vez pero desde casa porque me dejé las capturas en el pc de clase");
     }
 }
